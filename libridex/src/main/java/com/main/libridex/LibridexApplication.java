53ee06ee4bbbc01d10192e3d4176308cc0ccf19f
Pablorv004 <pablorv004@gmail.com>
2024-11-19 13:46:13 +0100

    Initialized project. Added important gitignore.

---
+++ libridex/src/main/java/com/main/libridex/LibridexApplication.java
@@ -0,0 +1,13 @@
+package com.main.libridex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibridexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibridexApplication.class, args);
+	}
+
+}
---
+++ libridex/src/test/java/com/main/libridex/LibridexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.libridex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibridexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
