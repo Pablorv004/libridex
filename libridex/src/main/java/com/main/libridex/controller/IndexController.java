797276a5ee51f8a74787c9edfc45f274031cee9c
Pablorv004 <pablorv004@gmail.com>
2024-11-20 14:12:41 +0100

    Added basic Book Service.

--- libridex/src/main/java/com/main/libridex/controller/IndexController.java
+++ libridex/src/main/java/com/main/libridex/controller/IndexController.java
@@ -1,14 +1,22 @@
 package com.main.libridex.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.main.libridex.service.BookService;
 
 @Controller
 public class IndexController {
 
     private static final String INDEX = "index";
     
+    @Autowired
+    @Qualifier("bookService")
+    private BookService bookService;
+
     @GetMapping("/")
     public String redirectToIndex() {
         return "redirect:/index";
@@ -16,6 +24,8 @@ public class IndexController {
 
     @GetMapping("/index")
     public ModelAndView index(){
-        return new ModelAndView(INDEX);
+        ModelAndView mav = new ModelAndView(INDEX);
+        mav.addObject("mostReservedBooks", bookService.findAll());
+        return mav;
     }
 }
