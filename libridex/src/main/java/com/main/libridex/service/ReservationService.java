f621f0fe81d493971656a86b79673725311d82ad
Pablorv004 <pablorv004@gmail.com>
2024-12-12 09:10:37 +0100

    minor qol changes

--- libridex/src/main/java/com/main/libridex/service/ReservationService.java
+++ libridex/src/main/java/com/main/libridex/service/ReservationService.java
@@ -2,7 +2,7 @@ package com.main.libridex.service;
 
 import com.main.libridex.entity.Reservation;
 
-public interface ResertavionService {
+public interface ReservationService {
     void save(Reservation reservation);
     void delete(Integer id);
     void getAllReservations();
